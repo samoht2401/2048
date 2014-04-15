361eace4ccd036157323bcf8ba726f3edb247e03
samoht2401 <thomasbauvent@gmail.com>
2014-04-15 18:00:40 +0200

    Inital test

--- 2048/2048.cpp
+++ 2048/2048.cpp
@@ -6,6 +6,11 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int tab[4][4] = {{0,0,0,0}, // pour [X,Y] : X vertical, Y horizontale
+					 {0,0,0,0}, // !!! La table est donc transposé (symétrique à la diagonale haut-gauche, bas-droite)
+					 {0,0,0,0},
+					 {0,0,0,0}};
+
 	return 0;
 }
 
